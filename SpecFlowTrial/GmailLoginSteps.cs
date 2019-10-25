52a1b1b30507980003e79a98cbc66715d22f5fd9
yogee540@gmail.com <yogee540@gmail.com>
2019-10-25 14:26:02 +0000

    Add project files.

---
+++ SpecFlowTrial/GmailLoginSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Calc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpecFlowTrial
+{
+    [Binding]
+    public class GmailLoginSteps
+    {
+        private int result;
+
+        private Calculator calculator = new Calculator();
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            //Conte
+            calculator.FirstNumber = number;
+            // ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+            //ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ SpecFlowTrial/GoogleSearchSteps.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow;
+
+namespace SeleniumAndSpecflow
+{
+    [Binding]
+    public class GoogleSearch
+    {
+        private readonly IWebDriver _webDriver;
+        private readonly IWait<IWebDriver> _defaultWait;
+        private string _searchTerm;
+
+        public GoogleSearch()
+        {
+            //string projectpath = PlatformServices.Default.Application.ApplicationBasePath;//AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            _webDriver = new ChromeDriver(@"C:\Users\P10454236\source\repos\SpecFlowTrial\SpecFlowTrial\bin\Debug\netcoreapp2.1\");
+
+            _defaultWait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(100)
+            };
+        }
+
+        [Given(@"I navigate to (.*)")]
+        public void Navigate(string url)
+        {
+            if (!url.StartsWith("http") && !url.StartsWith("https"))
+                url = "https://" + url;
+
+            _webDriver.Navigate().GoToUrl(url);
+            _webDriver.Manage().Window.Maximize();
+        }
+
+        [When(@"I search for (.*)")]
+        public void Search(string searchTerm)
+        {
+            _searchTerm = searchTerm;
+
+            IWebElement searchInput = _webDriver.FindElement(By.CssSelector("input[name='q']"));
+            searchInput.SendKeys(searchTerm);
+
+            IWebElement searchForm = _webDriver.FindElement(By.CssSelector("form[action=\"/search\"]"));
+            searchForm.Submit();
+        }
+
+        [Then(@"Google should return valid search results")]
+        public void ValidateSearchResults()
+        {
+            
+            var results = _webDriver.FindElement(By.CssSelector("input[name='q']"));
+            string pageData = results.GetAttribute("innerText");
+           
+
+            Assert.IsNotNull(pageData);
+
+            IWebElement resultsDiv =
+                _defaultWait.Until(
+                    ExpectedConditions.ElementExists(By.CssSelector($"div[data-async-context=\"query:{_searchTerm}\"]")));
+
+            Assert.IsNotNull(resultsDiv.Text);
+        }
+
+        [AfterScenario]
+        public void CleanUp()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
